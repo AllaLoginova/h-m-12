341846e27e11ffdb38ce9c431ec966bbab6327a7
AllaLoginova <122536827+AllaLoginova@users.noreply.github.com>
2024-09-04 15:16:25 +0300

    Add files via upload

---
+++ home_work_12/scripts/index.js
@@ -0,0 +1,34 @@
+const slider = document.getElementById('myRange');
+const element = document.getElementsByClassName("slidecontainer");
+const el_red = document.getElementById("red");
+const el_green = document.getElementById("green");
+slider.addEventListener('input', function () {
+    const percentage = slider.value;
+    // element.style.background = `linear-gradient(to right, black ${percentage}%, white ${percentage}%)`;
+
+    el_red.style.background = `linear-gradient(to right, red ${percentage}%, white ${percentage}%)`;
+    el_green.style.background = `linear-gradient(to left, green ${percentage}%, white ${percentage}%)`;
+    
+    el_red.style.marginLeft = 10 + percentage*3 + 'px';
+    el_green.style.marginLeft = 310 - percentage*3 + 'px';
+    
+});
+
+
+
+// function sizePic() {
+//     size = document.getElementById("myRange").value;
+//     img = document.getElementById("red");
+//     img.innerHTML = size
+    
+//     img.width = 60 + 20*size;
+// }
+
+// let rangeslider = document.getElementById("myRange");
+// let output = document.getElementById("red");
+// output.innerHTML = rangeslider.value;
+
+// rangeslider.oninput = function () {
+//     output.style.width += this.value;
+// }
+
